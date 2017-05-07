9b374480071ce0aa50055f6e1b442d11516f8a4f
Ramiro Jr. Franco <rjfranco@gmail.com>
2017-05-07 13:27:52 -0700

    Simplying return, removing string interprolation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,17 +1,13 @@
 // This is where it all goes :)
 
 function parseEmailAddress(text) {
-  parsedEmail = text.replace('- at -', '@')
-                    .replace('punto', '.')
-                    .replace(/\s/g, '')
-                    .replace(/\(.*\)/, '');
-  return parsedEmail;
+  return text.replace('- at -', '@').replace('punto', '.').replace(/\s/g, '').replace(/\(.*\)/, '');
 }
 
 function replaceEmail(index, element) {
   var $element = $(element);
   var emailAddress = parseEmailAddress($element.text());
-  var $newElement = $(`<a href=mailto:${emailAddress}>${emailAddress}</a>`);
+  var $newElement = $('<a href="mailto:' + emailAddress + '">' + emailAddress + '</a>');
 
   $element.replaceWith($newElement);
 }
