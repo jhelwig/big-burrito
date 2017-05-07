0111b6552d310a42ac137f34babe9da5abb796fb
Ramiro Jr. Franco <rjfranco@gmail.com>
2017-05-07 12:34:12 -0700

    Adding a little JS to act as a spam filter…

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,25 @@
 // This is where it all goes :)
+
+function parseEmailAddress(text) {
+  parsedEmail = text.replace('- at -', '@')
+                    .replace('punto', '.')
+                    .replace(/\s/g, '')
+                    .replace(/\(.*\)/, '');
+  return parsedEmail;
+}
+
+function replaceEmail(index, element) {
+  let $element = $(element)
+  let emailAddress = parseEmailAddress($element.text());
+  let $newElement = $(`<a href=mailto:${emailAddress}>${emailAddress}</a>`)
+
+  $element.replaceWith($newElement);
+}
+
+function replaceEmails() {
+  $('.email').each(replaceEmail);
+}
+
+$(document).ready(function() {
+  replaceEmails();
+});
